a11203b7c5df45b79d1fe53e40fa3c7090d7bd54
cristovao1985 <cristovaojandson@gmail.com>
2021-01-29 01:36:19 -0300

    criada rota de produtos

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductsComponent} from "./views/products/products.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'produtos',component: ProductsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
